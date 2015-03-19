cdc79b7d0dd8c403111713df0dc38d923f07c3ba
李双祥 <lyxtime@163.com>
2015-03-19 17:51:31 +0800

    修改视频列表界面

--- NetworkTeachPlatform/src/main/java/com/chzu/ntp/ui/CourseVideoFragment.java
+++ NetworkTeachPlatform/src/main/java/com/chzu/ntp/ui/CourseVideoFragment.java
@@ -6,6 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.chzu.ntp.adapter.CoursevideoAdapter;
+import com.chzu.ntp.model.Coursevideo;
+import com.chzu.ntp.model.Courseware;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -14,6 +22,8 @@ import android.view.ViewGroup;
 public class CoursevideoFragment extends Fragment {
 
     private static CoursevideoFragment mCoursevideoFragment;
+    private ListView mCoursevideoList;
+    private CoursevideoAdapter mCoursevideoAdapter;
 
     /**
      * 创建单例对象
@@ -28,7 +38,23 @@ public class CoursevideoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_course_video, container, false);
+        View view=inflater.inflate(R.layout.fragment_course_video, container, false);
+        mCoursevideoList= (ListView) view.findViewById(R.id.coursevideList);
+        mCoursevideoAdapter=new CoursevideoAdapter(getData(),getActivity().getApplicationContext());
+        mCoursevideoList.setAdapter(mCoursevideoAdapter);
+        return view;
+    }
+
+    /**
+     * 模拟数据
+     */
+    public List<Coursevideo> getData(){
+        List<Coursevideo> list=new ArrayList<Coursevideo>();
+        for (int i = 0; i < 5; i++) {
+            Coursevideo coursevideo=new Coursevideo(""+i,"第一集");
+            list.add(coursevideo);
+        }
+        return list;
     }
 
 
