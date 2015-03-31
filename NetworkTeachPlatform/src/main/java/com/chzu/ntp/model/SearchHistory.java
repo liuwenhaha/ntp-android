b1fc581606366bf29d00a3a136eb9562f52eb454
yanxing <lyxtime@163.com>
2015-04-01 00:40:23 +0800

    add file

---
+++ NetworkTeachPlatform/src/main/java/com/chzu/ntp/model/SearchHistory.java
@@ -0,0 +1,20 @@
+package com.chzu.ntp.model;
+
+/**
+ * 搜索历史
+ *
+ * @author yanxing
+ */
+public class SearchHistory {
+
+    private String content;//搜索内容
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
